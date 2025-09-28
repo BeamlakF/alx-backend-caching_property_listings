8ad1f773bbe1eb564a9466bee8d3f38394e392c0
BeamlakF <beamlakfekadu562@gmail.com>
2025-09-28 16:01:01 +0300

    task 1

--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-from .models import Property
-from .utils import get_all_properties
 from django.http import JsonResponse
+from .models import Property
 
 @cache_page(60 * 15)  # 15 minutes
 def property_list(request):
-    properties = get_all_properties()
-    data = list(properties.values())
-    return JsonResponse(data, safe=False)
+    properties = Property.objects.all()
+    data = list(properties.values())  # convert queryset to list of dicts
+    return JsonResponse({"properties": data})  # return as JSON object
